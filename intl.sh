f59a5ea58b6f5d8d3b0b760da86d6a24a13dd413
aditya aphale <aaphale3@gmail.com>
2020-07-12 19:24:19 -0400

    Modified closing comment.

--- intl.sh
+++ intl.sh
@@ -6,4 +6,4 @@ echo "Thank you for entering $LANGUAGE."
 echo "I will now send a memo to the ${LANGUAGE}_embassy."
 touch "${LANGUAGE}_embassy"
 
-echo "Program now closing."
+echo "This program is now closing."
