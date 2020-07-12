133e35eb707f9e125fc6f29a91af6fbe52dcc0d9
aditya aphale <aaphale3@gmail.com>
2020-07-12 19:04:46 -0400

    First git bash script

---
+++ intl.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Please enter your language of choice."
+read LANGUAGE
+echo "Thank you for entering $LANGUAGE."
+echo "I will now send a memo to the ${LANGUAGE}_embassy."
+touch "${LANGUAGE}_embassy"
